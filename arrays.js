1f0a4809baab25b229eb90167e289b946701ac16
Napoleon Guardado <napo.gv@hotmail.com>
2018-01-04 05:45:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+addElementToBeginningOfArray (chocolateBars, crunch){
+chocolateBars.unshift("crunch")
+}
+
+accessElementInArray(chocolateBars, 0){
+console.log(chocolateBars[0])
+}
